8b18178a3ae9eaaadb9f8409b4c2118455c1abdf
AI1411 <goethe0719@gmail.com>
2022-05-01 19:26:21 +0900

    シーザー暗号 リファクタリング

--- src/caesar_enc/caesar_enc.rs
+++ src/caesar_enc/caesar_enc.rs
@@ -1,17 +1,10 @@
 fn encrypt(text: &str, shift: i16) -> String {
-    let code_a = 'A' as i16;
-    let code_z = 'Z' as i16;
+    let a = 'A' as i16;
+    let is_az = |c| 'A' <= c && c <= 'Z';
+    let conv = |c| (((c - a + shift + 26) % 26 + a) as u8) as char;
+    let enc1 = |c| if is_az(c) { conv(c as i16) } else { c };
 
-    let mut result = String::new();
-
-    for ch in text.chars() {
-        let mut code = ch as i16;
-        if code_a <= code && code <= code_z {
-            code = (code - code_a + shift + 26) % 26 + code_a;
-        }
-        result.push((code as u8) as char);
-    }
-    return result;
+    return text.chars().map(|c| enc1(c)).collect();
 }
 
 fn main() {
