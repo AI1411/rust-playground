d35895bd882f358f8346e56150f1f87ef6abbe78
AI1411 <goethe0719@gmail.com>
2022-05-01 11:11:21 +0900

    九九 refactor

--- src/kuku/kuku.rs
+++ src/kuku/kuku.rs
@@ -1,8 +1,6 @@
 fn main() {
     for y in 1..10 {
-        for x in 1..10 {
-            print!("{:3}", x * y);
-        }
-        println!("");
+        let s = (1..10).map(|x| format!("{:3}", x * y)).collect::<Vec<String>>().join(",");
+        println!("{}", s)
     }
 }
