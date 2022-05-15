92c23c2f2a8dc0e6a7f4ee0c759feed559a48fd9
AI1411 <goethe0719@gmail.com>
2022-05-15 17:36:20 +0900

    テストコード

--- src/lib.rs
+++ src/lib.rs
@@ -28,4 +28,28 @@ mod tests {
 
         assert_eq!(v1, v2);
     }
+
+    #[derive(Debug,PartialEq)]
+    struct GItem {
+        name: String,
+        price: i64,
+    }
+
+    #[test]
+    fn item_test() {
+        let apple1 = GItem {
+            name: String::from("apple").to_string(),
+            price: 2400,
+        };
+        let mut apple2 = GItem {
+            name: "apple".to_string(),
+            price: 0,
+        };
+        apple2.price = 2400;
+
+        assert_eq!(apple1.name, apple2.name);
+        assert_eq!(apple2.price, apple2.price);
+
+        assert_eq!(apple1, apple2);
+    }
 }
